a89d906e1ff7a68221094a5bc0f69f2d07382074
AxelRey777 <159395193+AxelRey777@users.noreply.github.com>
2024-10-16 17:33:29 -0700

    CORREGIDO Reyes Aguilar Axel 16/10/2024

--- BaseDeDatosWeb/Parcial2/practica9/practica9.js
+++ BaseDeDatosWeb/Parcial2/practica9/practica9.js
@@ -1,16 +1,17 @@
   $(document).ready ( function(){
     let cartas =new Array(54).fill(false);
         //funcion para dar la carta :o
+        var cont = 0;
         $("#dar_carta").click(function(){
             var b = true;
-            var cont = 0;
+            
             while(b){
             var num = Math.floor(Math.random()*54)+1;
                     console.log("numero de cartas"+num);
                 if(cartas[num-1] == true){
                 continue;
                 }else{
-                    $("#carta_activa").html("<img src='loteria/"+num+".jpg>");
+                    $("#carta_activa").html("<img src=loteria/"+num+".jpg>");
                     cartas[num-1] = true;
                     b=false;
                     cont++;
